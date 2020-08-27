9532b5e557e2f572f7ca1292cfdc38c8642f6dff
GuilhermeBragaR <guilhermerbraga10@hotmail.com>
2020-08-26 21:54:18 -0300

    Conec banco de dados

--- index.php
+++ index.php
@@ -1,3 +1,16 @@
 <?php
-    echo "Boa noite";
+    //echo "Boa noite";
+    $con = mysqli_connect(
+        "localhost",
+        "root",
+        "123456",
+        "syspacientes");
+    if(!$con){
+        echo "Error: ".PHP.EOL;
+        exit;
+    }
+    echo "Conectou";
+    echo "<br>";
+    echo "Informacao do host: ". mysqli_get_host_info($con);
+    mysqli_close($con);  
 ?>
